e39df90f3ebbda9e8fe763db0dcb81860b6772e7
0verTaker <renanneves430@gmail.com>
2024-05-11 23:48:21 -0300

    new

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -127,6 +127,7 @@ int main(int argc, char **argv)
     printf("Altura da arvore: %d\n", AlturaArv(Arv));
     printf("Grau de cada nó em percurso simétrico:\n");
     GrauSim(Arv);
+    printf("teste");
     DestroiArvore(&Arv);
 
     return 0;
